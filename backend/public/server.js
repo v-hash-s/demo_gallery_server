5bc464e3ae83d17fee3dd87761edfd91edf484db
vlasta_stelmakh <vlasta.stelmakh2.0@gmail.com>
2021-09-16 16:57:05 +0300

    feat: create server
    create server and basic routing between gallery and login pages

--- backend/public/server.js
+++ backend/public/server.js
@@ -1,35 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const http_1 = require("http");
-const port = 5000;
-const loginPage = (0, fs_1.readFileSync)('/Users/user/Desktop/nodejs_gallery/frontend/index.html');
-const loginJs = (0, fs_1.readFileSync)('/Users/user/Desktop/nodejs_gallery/frontend/public/login.js');
-const server = (0, http_1.createServer)((req, res) => {
-    const url = req.url;
-    try {
-        if (url === '/login') {
-            res.writeHead(200, { 'content-type': 'text/html' });
-            res.write(loginPage);
-            res.end(console.log('loaded html'));
-        }
-        if (url === '/login.js') {
-            res.writeHead(200, { 'content-type': 'text/javascript' });
-            res.write(loginJs);
-            res.end(console.log('loaded js'));
-        }
-        if (url === '/gallery') {
-        }
+const http = require('http');
+const fs = require('fs');
+let server = http.createServer((req, res) => {
+    let path = '../frontend';
+    switch (req.url) {
+        case '/login':
+            path += '/index.html';
+            break;
+        case '/gallery':
+            path += '/public/gallery.html';
+            break;
+        default:
+            console.log('nope');
+            break;
     }
-    catch (err) {
-        throw new Error(err);
-    }
-    // const url = req.url;
-    // if(url === '/login'){
-    //   res.write(loginPage);
-    //   res.end();
-    // }
-    // if(url === '/gallery'){
-    // }
-}).listen(port);
-console.log(loginJs);
+    console.log(req.url, req.method);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    });
+}).listen(3000, '127.0.0.1');
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,36 @@
-import { readFileSync } from 'fs';
-import { createServer, IncomingMessage, ServerResponse } from 'http';
- 
-const port = 5000;
-const loginPage = readFileSync('/Users/user/Desktop/nodejs_gallery/frontend/index.html');
-const loginJs = readFileSync('/Users/user/Desktop/nodejs_gallery/frontend/public/login.js')
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  const url = req.url;
 
-  try{
-    if(url === '/login'){
-      res.writeHead(200, {'content-type': 'text/html'})
-      res.write(loginPage);
-      res.end(console.log('loaded html'));
-    }
+import { IncomingMessage, ServerResponse } from "http";
+const http = require('http');
+const fs = require('fs');
 
-    if(url === '/login.js'){
-      res.writeHead(200, {'content-type': 'text/javascript'});
-      res.write(loginJs);
-      res.end(console.log('loaded js'));
-    }
-  
-    if(url === '/gallery'){
-  
-    }
-  } catch(err: any){
-    throw new Error(err)
+let server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+
+  let path: string = '../frontend';
+  switch(req.url){
+    case '/login':
+      path += '/index.html'
+      break;
+    case '/gallery':
+      path += '/public/gallery.html'
+      break;
+    default: 
+      console.log('nope')
+      break;
   }
-  // const url = req.url;
-  // if(url === '/login'){
-  //   res.write(loginPage);
-  //   res.end();
-  // }
 
-  // if(url === '/gallery'){
-
-  // }
-
+  console.log(req.url, req.method)
+  res.writeHead(200, {'Content-Type': 'text/html'});
   
-}).listen(port);
+
  
+    fs.readFile(path, 
+    (err: any, data: any) =>{
+      if(err) {
+        console.log(err);
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  
+}).listen(3000, '127.0.0.1')
