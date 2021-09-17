07bd7ddedce28d3030eebb0fd3f5c21bd550a1b8
vlasta_stelmakh <vlasta.stelmakh2.0@gmail.com>
2021-09-17 15:28:39 +0300

    feat: finish draft authorization
    add authorization. Validate user's data and send back token if the data is correct according to inner userDB

--- backend/public/server.js
+++ backend/public/server.js
@@ -1,29 +1,74 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const serverGallery_js_1 = require("./serverGallery.js");
 const http = require('http');
-const fs = require('fs');
-let server = http.createServer((req, res) => {
-    let path = '../frontend';
-    switch (req.url) {
-        case '/login':
-            path += '/index.html';
-            break;
-        case '/gallery':
-            path += '/public/gallery.html';
-            break;
-        default:
-            console.log('nope');
-            break;
+const fs = require("fs");
+const path = require("path");
+const token = {
+    'token': 'token',
+};
+const users = {
+    'asergeev@flo.team': 'jgF5tn4F',
+    'vkotikov@flo.team': 'po3FGas8',
+    'tpupkin@flo.team': 'tpupkin@flo.team',
+};
+const regex = /^\b([0-9A-Z])+\b$/gi;
+class User {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+        this.email = email;
+        this.password = password;
     }
-    console.log(req.url, req.method);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-        }
+    isCorrectPassword() {
+        if (this.password.match(regex))
+            return true;
         else {
-            res.write(data);
-            res.end();
+            alert("Invalid form of password");
+            return false;
         }
-    });
-}).listen(3000, '127.0.0.1');
+    }
+}
+const requestListener = function (req, res) {
+    let url = req.url;
+    if (req.method == "OPTIONS") {
+        //In case of an OPTIONS, we allow the access to the origin of the petition
+        res.setHeader('Access-Control-Allow-Methods', "PUT,PATCH,DELETE,POST,GET");
+        res.setHeader("Access-Control-Allow-Headers", "API-Key,Content-Type,If-Modified-Since,Cache-Control,Access-Control-Allow-Methods, Authorization");
+        res.setHeader("Access-Control-Max-Age", "86400");
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200);
+        res.end();
+    }
+    if (req.method === "POST") {
+        let body = "";
+        req.on("data", (data) => {
+            body += data.toString();
+            console.log(body);
+        });
+        //res.end(JSON.stringify(token))
+        req.on("end", () => {
+            let user_temp = JSON.parse(body);
+            // let user = new User(user_temp.email, user_temp.password);
+            if (user_temp.email in users && user_temp.password === users[user_temp.email]) {
+                console.log(JSON.stringify(token));
+                console.log(req.method);
+                //res.end('dddd')
+                res.setHeader('Access-Control-Allow-Methods', "PUT,PATCH,DELETE,POST,GET");
+                res.setHeader("Access-Control-Allow-Headers", "API-Key,Content-Type,If-Modified-Since,Cache-Control,Access-Control-Allow-Methods, Authorization");
+                res.setHeader("Access-Control-Max-Age", "86400");
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.writeHead(200);
+                res.end(JSON.stringify(token));
+            }
+        });
+    }
+    else if (req.method === "GET") {
+        (0, serverGallery_js_1.sendGalleryObject)(url);
+    }
+    // else if (url === "/static") {
+    // }
+};
+const server = http.createServer(requestListener);
+console.log("I'm started");
+server.listen(8080);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,36 +1,97 @@
-
 import { IncomingMessage, ServerResponse } from "http";
+import { sendGalleryObject } from "./serverGallery.js"
 const http = require('http');
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
-let server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 
-  let path: string = '../frontend';
-  switch(req.url){
-    case '/login':
-      path += '/index.html'
-      break;
-    case '/gallery':
-      path += '/public/gallery.html'
-      break;
-    default: 
-      console.log('nope')
-      break;
-  }
+interface UsersDB {
+  [k: string]: string;
+}
 
-  console.log(req.url, req.method)
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
+interface Token {
+  [k: string]: string;
+} 
 
- 
-    fs.readFile(path, 
-    (err: any, data: any) =>{
-      if(err) {
-        console.log(err);
-      } else {
-        res.write(data);
+const token: Token = {
+  'token': 'token',
+}
+
+const users: UsersDB = {
+  'asergeev@flo.team': 'jgF5tn4F',
+  'vkotikov@flo.team': 'po3FGas8',
+  'tpupkin@flo.team': 'tpupkin@flo.team',
+}
+
+const regex = /^\b([0-9A-Z])+\b$/gi;
+class User {
+    constructor(private email: string, private password: string) {
+        this.email = email;
+        this.password = password;
+    }
+    isCorrectPassword() {
+        if (this.password.match(regex))
+            return true;
+        else {
+            alert("Invalid form of password");
+            return false;
+        }
+    }
+}
+
+const requestListener = function (req: IncomingMessage, res: ServerResponse) {
+  let url = req.url;
+
+  if (req.method == "OPTIONS")
+    {
+        //In case of an OPTIONS, we allow the access to the origin of the petition
+        res.setHeader('Access-Control-Allow-Methods', "PUT,PATCH,DELETE,POST,GET")
+        res.setHeader("Access-Control-Allow-Headers", "API-Key,Content-Type,If-Modified-Since,Cache-Control,Access-Control-Allow-Methods, Authorization")
+        res.setHeader("Access-Control-Max-Age", "86400")
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.writeHead(200);
         res.end();
-      }
-    });
-  
-}).listen(3000, '127.0.0.1')
+    }
+  if (req.method === "POST") {
+    
+        let body = "";
+        req.on("data", (data) => {
+            body += data.toString();
+            console.log(body);
+
+
+        });
+
+        
+//res.end(JSON.stringify(token))
+        req.on("end", () => {
+          
+            let user_temp = JSON.parse(body);
+            // let user = new User(user_temp.email, user_temp.password);
+            if (user_temp.email in users && user_temp.password === users[user_temp.email]) {
+              console.log(JSON.stringify(token));
+              console.log(req.method);
+              //res.end('dddd')
+              res.setHeader('Access-Control-Allow-Methods', "PUT,PATCH,DELETE,POST,GET")
+              res.setHeader("Access-Control-Allow-Headers", "API-Key,Content-Type,If-Modified-Since,Cache-Control,Access-Control-Allow-Methods, Authorization")
+              res.setHeader("Access-Control-Max-Age", "86400")
+              res.setHeader('Access-Control-Allow-Origin', '*')
+              res.writeHead(200);
+              res.end(JSON.stringify(token));
+            } 
+        });
+    
+  }
+  else if (req.method === "GET") {
+    sendGalleryObject(url)
+  }
+  // else if (url === "/static") {
+
+  // }
+
+}
+
+const server = http.createServer(requestListener);
+console.log("I'm started");
+server.listen(8080);
+
